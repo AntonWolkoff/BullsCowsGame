1f404c006b10794709aa22d146ae2028f13568c7
Ryker <wolkoffanton@gmail.com>
2020-07-07 23:03:34 +0300

    Set up the basic core loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,27 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT(" Hello there! \n You are on the BullCow game. \n Pls press the Enter button!"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    FString HiddenWord = TEXT("isogram");
+    PrintLine(TEXT("Your input is " + Input));
+    PrintLine(CheckInput(Input, HiddenWord));
+
+
+}
+
+FString UBullCowCartridge::CheckInput(FString Input, FString Isogram)
+{
+    PrintLine(Input + " " + Isogram);
+    if (Input == Isogram) {
+        return TEXT("VICTORY!");
+    }
+    else {
+        return TEXT("LOOSER!");
+    }
 }
